a0e2de0081ad024a61eb90ae27448531fe22197d
handler-bird <powerk11@hotmail.de>
2025-06-07 22:44:34 +0200

    slurm script for precomputing

---
+++ slurm/embedding.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH --partition=NvidiaAll
+
+python precompute.py --data_dir outputs/baseline-train-indexed.pkl --output_dir outputs/clap-train-embeddings.pkl
+
+python precompute.py --data_dir outputs/baseline-valid-indexed.pkl --output_dir outputs/clap-valid-embeddings.pkl --indexing
