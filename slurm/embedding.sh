511c83ab08ff4fcbcaa0301e017a468077bcc205
handler-bird <powerk11@hotmail.de>
2025-06-15 19:12:55 +0200

    slurm script for precomputing embeddings

--- slurm/embedding.sh
+++ slurm/embedding.sh
@@ -3,4 +3,4 @@
 
 python precompute.py --data_dir outputs/baseline-train-indexed.pkl --output_dir outputs/clap-train-embeddings.pkl
 
-python precompute.py --data_dir outputs/baseline-valid-indexed.pkl --output_dir outputs/clap-valid-embeddings.pkl --indexing
+#python precompute.py --data_dir outputs/baseline-valid-indexed.pkl --output_dir outputs/clap-valid-embeddings.pkl
