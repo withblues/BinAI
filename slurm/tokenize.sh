cd74f001eb8c314540fdd660dcac8df50841de41
handler-bird <powerk11@hotmail.de>
2025-06-15 19:09:59 +0200

    slurm script for tokenizing

--- slurm/tokenize.sh
+++ slurm/tokenize.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=Krater
 
-#python tokenize_data.py --data_dir outputs --output_dir outputs
+python tokenize_data.py --data_dir outputs --output_dir outputs --checkpoint_dir outputs
 
-python tokenize_data.py --data_dir outputs --output_dir outputs --split valid
+#python tokenize_data.py --data_dir outputs --output_dir outputs --checkpoint_dir outputs --split valid
