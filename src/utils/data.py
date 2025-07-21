4f04fdca323cfac5f050fd121fdcfad09c68e490
handler-bird <powerk11@hotmail.de>
2025-07-21 18:24:59 +0200

    add helper functions for calculating metrics / data

--- src/utils/data.py
+++ src/utils/data.py
@@ -1,7 +1,8 @@
 import os
 import pickle
 import json
-
+import pandas as pd
+from datasets import load_from_disk
 
 def load_data(dir_path: str):
     binary = os.path.join(dir_path)
@@ -16,3 +17,26 @@ def load_json(dir_path: str):
         data = json.load(f)
 
     return data
+
+def load_df_and_get_model_name(filepath):
+    df = pd.read_csv(filepath)
+    model_name = os.path.basename(filepath).split("-results")[0]
+
+    print(f'loaded dataframe {filepath}')
+    return df, model_name
+
+def load_dataset_and_get_model_name(filepath):
+    dataset = load_from_disk(filepath)
+    model_name = os.path.basename(filepath).split("-test")[0]
+
+    print(f'loaded dataset {filepath}')
+    return dataset, model_name
+
+def load_json_and_get_model_name(filepath):
+    with open(filepath) as f:
+        data = json.load(f)
+
+    model_name = os.path.basename(filepath).split("-metadata")[0]
+
+    print(f'loaded dataset {filepath}')
+    return data, model_name
