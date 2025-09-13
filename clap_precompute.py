4883898ed8210d22af086219aba41955a84d1371
withblues <powerk11@hotmail.de>
2025-09-13 21:56:26 +0200

    option to set max_len of the tokenizer

--- clap_precompute.py
+++ clap_precompute.py
@@ -13,11 +13,15 @@ class PreTrainedModel:
         self,
         model_path: str,
         device: str,
+        max_len: int,
     ):
         self.device = device
         self.asm_tokenizer = AutoTokenizer.from_pretrained(
             model_path, trust_remote_code=True
         )
+
+        self.asm_tokenizer.model_max_length = max_len
+
         self.asm_encoder = AutoModel.from_pretrained(
             model_path, trust_remote_code=True
         ).to(device)
@@ -28,7 +32,7 @@ class PreTrainedModel:
 
     def forward(self, batch):
         asm_input = self.asm_tokenizer(
-            batch, padding=True, return_tensors="pt"
+            batch, return_tensors="pt",
         ).to(self.device)
         asm_embeddings = self.asm_encoder(**asm_input)
 
@@ -53,6 +57,7 @@ def compute_embeddings(batch, model):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Command line parameters")
     parser.add_argument("--data_dir", required=True)
+    parser.add_argument("--max_len", default=1024, type=int)
     args = parser.parse_args()
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -60,7 +65,7 @@ if __name__ == "__main__":
     dataset = load_from_disk(dataset_path)
 
     # load model
-    teacher_model = PreTrainedModel("hustcw/clap-asm", device)
+    teacher_model = PreTrainedModel("hustcw/clap-asm", device, args.max_len)
 
     # 1. Define a dedicated directory for cache files using an absolute path.
     cache_dir = os.path.join(args.data_dir, ".cache")
@@ -69,10 +74,10 @@ if __name__ == "__main__":
     os.makedirs(cache_dir, exist_ok=True)
 
     # 3. Create the full, unambiguous path to the cache file.
-    cache_file_path = os.path.join(cache_dir, "clap_embeddings.arrow")
+    cache_file_path = os.path.join(cache_dir, f"clap_embeddings_{args.max_len}.arrow")
 
     # create embeddings
     dataset = dataset.map(compute_embeddings, batched=True, batch_size=256, cache_file_name=cache_file_path, fn_kwargs={"model": teacher_model})
 
-    dataset.save_to_disk(dataset_path)
+    dataset.save_to_disk(dataset_path + f'_{args.max_len}')
     print("precomputing done")
